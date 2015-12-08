36eaea7f8fbca4e82d52f47dab129d8226e15c47
Quentin Cerny <quentin.cerny@gmail.com>
2015-12-08 19:08:34 +0100

    Bootstraps Symfony bundle

---
+++ src/Majora/Bundle/OrientDbGraphBundle/MajoraOrientDbGraphBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Majora\Bundle\OrientDbGraphBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle class
+ */
+class MajoraOrientDbGraphBundle extends Bundle
+{
+
+}
